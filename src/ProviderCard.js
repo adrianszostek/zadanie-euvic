0419350a894060b194bfd0dfdd8f2a04b4433f51
adrianszostek <69784002+adrianszostek@users.noreply.github.com>
2022-03-21 22:37:45 +0100

    Added proper date in the cards with abbreviations of the weekdays and months

--- src/ProviderCard.js
+++ src/ProviderCard.js
@@ -6,12 +6,20 @@ class ProviderCard extends Component {
 
     render() {
         const provider = this.props.providerData;
+        /* format the date and get short names of the months and weekdays */
+        const monthsNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        const daysNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+        const date = provider.dates.date;
+        const dateYear = date.slice(0, 4);
+        const dateMonth = date.slice(5, 7);
+        const dateDay = date.slice(8);
+        const dateFormat = new Date(parseInt(dateYear), parseInt(dateMonth) - 1, parseInt(dateDay));
         return (
             <div className="provider-area">
                 <div className="provider-card">
                     <p className='date'>
-                        <span className="date-year">{2022}</span>
-                        Tue, 14 Mar
+                        <span className="date-year">{dateYear}</span>
+                        {daysNames[dateFormat.getDay() - 1]},<br />{monthsNames[dateFormat.getMonth()] + " " + dateDay}
                     </p>
                     <div className='provider-info'>
                         <div className='provider-name-and-rating'>
