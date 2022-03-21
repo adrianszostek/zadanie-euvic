685d6ac58b821f97300a95b74f45f9186b45ffc5
adrianszostek <69784002+adrianszostek@users.noreply.github.com>
2022-03-21 22:22:05 +0100

    Added API fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Logo from './logo.js';
 import './App.css';
 import React, { Component } from 'react'
+import ProviderCard from './ProviderCard';
 import { Menu, Search, AccountCircle } from '@mui/icons-material'
 
 class App extends Component {
+  state = {
+    providers: []
+  }
+  componentDidMount() {
+    /* request the data from the NFZ API */
+    let filteredUrl = 'https://api.nfz.gov.pl/app-itl-api/queues?page=1&limit=25&format=json&case=1&province=12&benefitForChildren=true&locality=GLIWICE&api-version=1.3'
+    fetch(filteredUrl)
+      .then(response => response.json())
+      .then(
+        result => {
+          this.setState({
+            providers: result.data
+          })
+        }
+      )
+  }
   render() {
     return (
       <div className="app">
