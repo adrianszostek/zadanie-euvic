1fb6ce104265a359d50db49109d88e04228edeb6
adrianszostek <69784002+adrianszostek@users.noreply.github.com>
2022-03-21 22:10:34 +0100

    Basic page structure with icons in the header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-// import logo from './logo.svg';
+import Logo from './logo.js';
 import './App.css';
+import React, { Component } from 'react'
+import { Menu, Search, AccountCircle } from '@mui/icons-material'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <div>
+            <Menu />
+            <div>
+              <Search />
+              <AccountCircle />
+            </div>
+          </div>
+          <Logo className="logo-graphics" />
+        </header>
+        <div className='providers'>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
