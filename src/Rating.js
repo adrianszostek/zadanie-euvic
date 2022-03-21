2be9c3fa4246e182eaace3034eb50f44ecbbd47a
adrianszostek <69784002+adrianszostek@users.noreply.github.com>
2022-03-21 22:44:42 +0100

    Rating stars are highlighted on mouse enter

--- src/Rating.js
+++ src/Rating.js
@@ -2,6 +2,21 @@ import { StarRate, StarRateOutlined } from '@mui/icons-material'
 import { Component } from 'react';
 
 class Rating extends Component {
+    constructor(props) {
+        super(props);
+        this.highlightStars = this.highlightStars.bind(this);
+        this.state = {
+            starsHighlighted: 1,
+            starsRated: 1
+        };
+
+    }
+    /* highlighting the stars when the mouse enter over the star icon */
+    highlightStars = index => {
+        this.setState({
+            starsHighlighted: index
+        });
+    }
     render() {
         /* generate the initial stars view */
         const generateStars = () => {
@@ -9,7 +24,9 @@ class Rating extends Component {
             ];
             for (let i = 1; i <= 5; i++) {
                 initialStars.push(
-                    <li key={i} >
+                    <li key={i}
+                        className={this.state.starsHighlighted >= i ? 'starHighlighted' : 'starEmpty'}
+                        onMouseEnter={() => this.highlightStars(i)} >
                         <StarRate />
                         <StarRateOutlined />
                     </li>
